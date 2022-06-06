627a99df0abffd5dac759c7bb7ac84b149fade1f
Matt Conway <matthewjosephconway@gmail.com>
2022-06-05 20:35:22 -0700

    Added Trail Renderer Time Controls

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    public float TrailRenderTime = 2.0f;
+
+    public TrailRenderer[] TrailRenderers;
+
     // Start is called before the first frame update
     private void Start()
     {
+        TrailRenderers = FindObjectsOfType<TrailRenderer>();
     }
+
+    private float _timeIncrement = 0.01f;
 
     // Update is called once per frame
     private void Update()
@@ -17,6 +24,26 @@ public class Controls : MonoBehaviour
         {
             Debug.Log("Quit Application");
             Application.Quit();
+        }
+
+        //increase trail render time
+        float tempTime = TrailRenderTime;
+        if (Input.GetKey(KeyCode.UpArrow)) { tempTime += _timeIncrement; Debug.Log("Up"); }
+        if (Input.GetKey(KeyCode.DownArrow)) { tempTime -= _timeIncrement; Debug.Log("Down"); }
+        if (TrailRenderTime > 5) { tempTime = 5; }
+        if (TrailRenderTime < 0) { tempTime = 0; }
+        if (tempTime != TrailRenderTime)
+        {
+            TrailRenderTime = tempTime;
+            UpdateTrailRenderers();
+        }
+    }
+
+    private void UpdateTrailRenderers()
+    {
+        foreach (TrailRenderer trail in TrailRenderers)
+        {
+            trail.time = TrailRenderTime;
         }
     }
 }
