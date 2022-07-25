b3fe634904d1b9e4a5c475104600f162438e78c4
LAM-CMLA <wahei@curiousminds.la>
2022-07-25 13:30:51 -0700

    Added Color Picker

---
+++ Assets/Color picker/HSVPicker/ExampleScripts/ColorPickerTester.cs
@@ -0,0 +1,35 @@
+﻿using HSVPicker;
+using UnityEngine;
+namespace HSVPickerExamples
+{
+    public class ColorPickerTester : MonoBehaviour 
+    {
+
+        public ColorPicker picker;
+        public TrailRenderer trail;
+
+        public Color Color = Color.red;
+        public bool SetColorOnStart = false;
+
+	    // Use this for initialization
+	    void Start () 
+        {
+            picker.onValueChanged.AddListener(color =>
+            {
+                trail.material.SetColor("_startColor", color);
+                Color = color;
+            });
+            trail.material.SetColor("_startColor", picker.CurrentColor);
+
+            if(SetColorOnStart) 
+            {
+                picker.CurrentColor = Color;
+            }
+        }
+	
+	    // Update is called once per frame
+	    void Update () {
+	
+	    }
+    }
+}
---
+++ Assets/Color picker/HSVPicker/ExampleScripts/PickColor.cs
@@ -0,0 +1,53 @@
+using HSVPicker;
+using UnityEngine;
+namespace HSVPickerExamples
+{
+    public class PickColor : MonoBehaviour
+    {
+
+        public ColorPicker startPicker;
+        public ColorPicker endPicker;
+        private TrailRenderer trail;
+
+        public Color Color1 = Color.red;
+        public Color Color2 = Color.red;
+        public bool SetColorOnStart = false;
+
+        // Use this for initialization
+        void Start()
+        {
+           
+            trail = GetComponent<TrailRenderer>();
+            //Gradient start color setup
+            startPicker.onValueChanged.AddListener(color =>
+            {
+                trail.sharedMaterial.SetColor("_startColor", color);
+                Color1 = color;
+            });
+            trail.sharedMaterial.SetColor("_startColor", startPicker.CurrentColor);
+
+            if (SetColorOnStart)
+            {
+                startPicker.CurrentColor = Color1;
+            }
+            //Gradient end color setup
+            endPicker.onValueChanged.AddListener(color =>
+            {
+                trail.sharedMaterial.SetColor("_endColor", color);
+                Color2 = color;
+            });
+            trail.sharedMaterial.SetColor("_endColor", endPicker.CurrentColor);
+
+            if (SetColorOnStart)
+            {
+                endPicker.CurrentColor = Color2;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
