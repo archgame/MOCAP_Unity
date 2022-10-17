01a07c082ed56bdfb3787e35a8a53307ae44d3bb
Matt Conway <matthewjosephconway@gmail.com>
2022-10-16 18:58:53 -0700

    Added Sensor Visualizers to Avatars (can change sensor colors)

--- Assets/_Scripts/CharacterManager.cs
+++ Assets/_Scripts/CharacterManager.cs
@@ -8,14 +8,22 @@ public class CharacterManager : MonoBehaviour
 {
     public GameObject[] Char0Avatars;
     public int Char0Index = 0;
+    public int Char0ColorIndex = 0;
+    public Material Char0Mat;
     public GameObject[] Char1Avatars;
     public int Char1Index = 0;
+    public int Char1ColorIndex = 0;
+    public Material Char1Mat;
+    public Color[] colors;
 
     // Start is called before the first frame update
     private void Start()
     {
         NextChar(0);
         NextChar(1);
+
+        NextColor(0);
+        NextColor(1);
 
         /*/ Wahei Color Picker Set Color Example
         ColorPicker[] pickers = GameObject.FindObjectsOfType<ColorPicker>(); Debug.Log("pickers: " + pickers.Length);
@@ -60,6 +68,29 @@ public class CharacterManager : MonoBehaviour
         {
             Char0Index = index;
         }
+    }
+
+    public void NextColor(int avatarIndex)
+    {
+        //get the character to act on
+        Material mat = Char0Mat;
+        int index = Char0ColorIndex;
+        if (avatarIndex == 1)
+        {
+            mat = Char1Mat;
+            index = Char1ColorIndex;
+        }
+
+        //update material colors
+        Color color = colors[index];
+        mat.color = color;
+        mat.SetColor("_EmissionColor", color);
+
+        //update global variable
+        index++; //get the next index for the next avatar
+        if (index >= colors.Length) { index = 0; } //reset index if we are at the last avatar
+        if (avatarIndex == 1) { Char1ColorIndex = index; }
+        else { Char0ColorIndex = index; }
     }
 
     public void SetSkinnedMeshChildren(GameObject go, bool set)
